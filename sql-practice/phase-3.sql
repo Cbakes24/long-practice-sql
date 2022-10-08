feb14c71753d4d6216cf6fe4d74ea9bfe72cc0cf
nathalieramos <natramos2292@gmail.com>
2022-10-08 13:45:36 -0500

    push

--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -1,5 +1,20 @@
 -- Find Hermione's cats
 -- Your code here
+SELECT * FROM cats 
+    JOIN cat_owners ON (cat_owners.cat_id = cats.id) 
+    JOIN owners ON (owners.id = cat_owners.owner_id)
+    WHERE owners.first_name = "Hermione";
 
 -- Find All the Toys for Hermione's cats
 -- Your code here
+SELECT * FROM toys 
+    JOIN cats ON (cats.id = toys.cat_id) 
+    JOIN cat_owners ON (cat_owners.cat_id = cats.id)
+    JOIN owners ON (owners.id = cat_owners.owner_id)
+    WHERE owners.first_name = "Hermione";
+
+SELECT * FROM toys 
+    JOIN cats ON (cats.id = toys.cat_id) 
+    JOIN cat_owners ON (cat_owners.cat_id = cats.id)
+    JOIN owners ON (owners.id = cat_owners.owner_id)
+    WHERE cats.birth_year = 2015;
